d2876dbd523df46bd0789af9e7e1b22b49e126b0
VJ <twjmy@msn.com>
2021-05-17 00:45:16 +0800

    Update 4108056020談維傑-hw5.asm

--- 4108056020談維傑-hw5.asm
+++ 4108056020談維傑-hw5.asm
@@ -1,22 +1,22 @@
-; Encryption Program               (Encrypt.asm)
-
 ; This program demonstrates simple symmetric
 ; encryption using the XOR instruction.
 
 INCLUDE Irvine32.inc
-KEY = 239     		; any value between 1-255
 BUFMAX = 128     	; maximum buffer size
 
 .data
-sPrompt  BYTE  "Enter the plain text: ",0
-sEncrypt BYTE  "Cipher text:          ",0
-sDecrypt BYTE  "Decrypted:            ",0
+sPrompt  BYTE  "plain text: ",0
+sKeypt   BYTE  "key: ",0
+sEncrypt BYTE  "Cipher text: ",0
+sDecrypt BYTE  "Decrypted: ",0
+key      BYTE   BUFMAX+1 DUP(0)
 buffer   BYTE   BUFMAX+1 DUP(0)
 bufSize  DWORD  ?
 
 .code
 main PROC
 	call	InputTheString		; input the plain text
+	call	InputTheKey	; input the key
 	call	TranslateBuffer	; encrypt the buffer
 	mov	edx,OFFSET sEncrypt	; display encrypted message
 	call	DisplayMessage
@@ -48,6 +48,43 @@ InputTheString PROC
 InputTheString ENDP
 
 ;-----------------------------------------------------
+InputTheKey PROC
+;
+; Prompts user for a key string. Saves the key to fit
+; the plaintext size.
+; Receives: nothing
+; Returns: nothing
+;-----------------------------------------------------
+	pushad
+	mov	edx, OFFSET sKeypt	; display the prompt
+	call	WriteString
+	mov	ecx, BUFMAX		; maximum character count
+	mov	edx, OFFSET key   ; point to the buffer
+	call	ReadString		; input the string
+
+	cmp	eax, bufSize		; compare key size and plaintext size
+	ja	L1		; if the key size is greater than plaintext, do nothing
+
+	mov	ebx, bufSize		; ebx: caculate the number leave to fill
+	sub	ebx, eax		; ebx = bufSize - keySize
+	mov	ecx, ebx		; loop counter
+	mov	esi, eax		; point to the end of the key
+
+L2:
+	sub	esi, eax
+	mov	dl, key[esi]		; dl: get the correspond key to fill
+	add	esi, eax
+	mov	key[esi], dl		; fill the correspond key
+	inc	esi		; point to next byte
+	loop	L2		; fill the key till fit plaintext size
+
+L1:
+	call	Crlf
+	popad
+	ret
+	InputTheKey ENDP
+
+;-----------------------------------------------------
 DisplayMessage PROC
 ;
 ; Displays the encrypted or decrypted message.
@@ -76,7 +113,8 @@ TranslateBuffer PROC
 	mov	ecx,bufSize		; loop counter
 	mov	esi,0			; index 0 in buffer
 L1:
-	xor	buffer[esi],KEY	; translate a byte
+	mov	bl, key[esi]	; get the correspond key of the byte
+	xor	buffer[esi],bl	; translate a byte
 	inc	esi				; point to next byte
 	loop	L1
 
